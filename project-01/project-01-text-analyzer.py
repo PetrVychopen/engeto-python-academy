f341f147468b919d8b13d01a54fea0e255086e0e
Petr Vychopeň <petr.vychopen@direct.cz>
2023-12-21 14:24:54 +0100

    check username and password

--- project-01/project-01-text-analyzer.py
+++ project-01/project-01-text-analyzer.py
@@ -5,3 +5,23 @@ author: Petr Vychopeň
 email: vychopen.petr@gmail.com
 discord: Petr V. Psycholino#0224
 """
+
+# NOTE: Not the best practice (security)
+
+registered_users = {
+    "bob": "123",
+    "ann": "pass123",
+    "mike": "password123",
+    "liz": "pass123",
+}
+
+
+# TODO: asterisks while typing
+
+user_username = str(input("Username: "))
+user_password = str(input("Password: "))
+
+if user_username in registered_users and user_password == registered_users[user_username]:
+    print("ok")
+else:
+    print("nok")
