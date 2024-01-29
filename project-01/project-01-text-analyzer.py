5ca9d5f436d17905660b1d6a991af60d14fc590e
Petr Vychopeň <petr.vychopen@direct.cz>
2024-01-29 18:48:51 +0100

    text selection

--- project-01/project-01-text-analyzer.py
+++ project-01/project-01-text-analyzer.py
@@ -21,7 +21,23 @@ registered_users = {
 user_username = str(input("Username: "))
 user_password = str(input("Password: "))
 
-if user_username in registered_users and user_password == registered_users[user_username]:
-    print("ok")
+separator = 40 * "-"
+
+if user_username in registered_users and \
+    user_password == registered_users[user_username]:
+    
+    print(separator)
+    print("Welcome to the app, bob", "We have 3 texts to be analyzed.", sep="\n")
+    print(separator)
 else:
-    print("nok")
+    print("unregistered user, terminating the program..")
+    quit()
+
+selected_number = int(input("Enter a number btw. 1 and 3 to select:"))
+
+if selected_number is not int() and selected_number not in range(1, 4):
+    print("Wrong input. Select number 1, 2 or 3.")
+    quit()
+
+
+print(separator)
