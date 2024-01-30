d82767d6bb5e5b3b121b272eedd03f866325adf1
Petr Vychopeň <petr.vychopen@direct.cz>
2024-01-30 19:16:51 +0100

    init text analyzer logic

--- project-01/project-01-text-analyzer.py
+++ project-01/project-01-text-analyzer.py
@@ -8,6 +8,8 @@ discord: Petr V. Psycholino#0224
 
 # NOTE: Not the best practice (security)
 
+from task_template import TEXTS
+
 registered_users = {
     "bob": "123",
     "ann": "pass123",
@@ -27,17 +29,38 @@ if user_username in registered_users and \
     user_password == registered_users[user_username]:
     
     print(separator)
-    print("Welcome to the app, bob", "We have 3 texts to be analyzed.", sep="\n")
+    print(f"Welcome to the app, {user_username}", 
+          "We have 3 texts to be analyzed.", sep="\n")
     print(separator)
+
 else:
     print("unregistered user, terminating the program..")
     quit()
 
-selected_number = int(input("Enter a number btw. 1 and 3 to select:"))
 
-if selected_number is not int() and selected_number not in range(1, 4):
+selected_number = input("Enter a number btw. 1 and 3 to select:")
+if selected_number.isdigit() and int(selected_number) in range(1, 4):
+    print(separator)
+else:
     print("Wrong input. Select number 1, 2 or 3.")
     quit()
 
+if selected_number == '1':
+    word_count = len(TEXTS[0].split())
+    print(f"There are {word_count} words in the selected text.")
 
-print(separator)
+# Pro vybraný text spočítá následující statistiky:
+
+# počet slov,
+# počet slov začínajících velkým písmenem,
+# počet slov psaných velkými písmeny,
+# počet slov psaných malými písmeny,
+# počet čísel (ne cifer),
+# sumu všech čísel (ne cifer) v textu.
+
+# There are 54 words in the selected text.
+# There are 12 titlecase words.
+# There are 1 uppercase words.
+# There are 38 lowercase words.
+# There are 3 numeric strings.
+# The sum of all the numbers 8510
