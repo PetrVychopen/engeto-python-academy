99a7d9de2379907b621a749ade570dae28d500a7
Petr <petr@Mirus-Air.home>
2024-02-06 18:18:42 +0100

    secret number generator

--- project-02/project-02-bulls-and-cows.py
+++ project-02/project-02-bulls-and-cows.py
@@ -5,3 +5,18 @@ author: Petr Vychopeň
 email: vychopen.petr@gmail.com
 discord: Petr V. Psycholino#0224
 """
+
+import random
+
+def generate_secret_number():
+    digits = random.sample(range(1, 10), 4)
+    return ''.join(map(str, digits))
+
+print("Hi there!")
+print("-----------------------------------------------")
+print("I've generated a random 4 digit number for you.")
+print("Let's play a bulls and cows game.")
+print("-----------------------------------------------")
+
+secret_number = generate_secret_number()
+print("Secret number:", secret_number)
