eb4f3175b74371efb41a6f1c8f97a06a032276fb
Petr Vychopeň <petr.vychopen@direct.cz>
2024-02-08 16:33:40 +0100

    main loop

--- project-02/project-02-bulls-and-cows.py
+++ project-02/project-02-bulls-and-cows.py
@@ -29,3 +29,35 @@ print("-----------------------------------------------")
 
 secret_number = generate_secret_number()
 print("Secret number:", secret_number)
+
+# Init for attempt counter
+attempts = 0
+
+while True:
+
+    # User prompt
+    guess = input("Enter a number: ")
+    attempts += 1
+
+    # Validity check for guessed number
+    if not guess.isdigit() or len(guess) != 4 or len(set(guess)) != 4 \
+            or guess[0] == '0':
+        print("Invalid guess. Please enter a 4-digit number with unique "
+              "digits and not starting with 0.")
+        continue
+
+    # Evaluation of guessed number
+    bulls = sum(1 for s, g in zip(secret_number, guess) if s == g)
+    cows = sum(min(secret_number.count(digit), guess.count(digit)) \
+               for digit in set(guess))
+    cows -= bulls
+
+    # Statistics of guessed number
+    print(f"{bulls} {'bull' if bulls == 1 else 'bulls'}, {cows} "
+          f"{'cow' if cows == 1 else 'cows'}")
+
+    # Check for "bullseye"
+    if bulls == 4:
+        print(f"Correct, you've guessed the right number\nin {attempts} "
+              f"{'guess' if attempts == 1 else 'guesses'}!")
+        break
