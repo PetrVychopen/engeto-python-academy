b4f55422cf43ef285a4d86484bcc2ed468bdd035
Petr Vychopeň <petr.vychopen@direct.cz>
2024-02-28 21:03:54 +0100

    url check

--- project-03/project-03-elections-scraper.py
+++ project-03/project-03-elections-scraper.py
@@ -5,5 +5,14 @@ author: Petr Vychopeň
 email: vychopen.petr@gmail.com
 discord: Petr V. Psycholino#0224
 """
-import requests # Knihovna pro HTTP požadavky
+import os
+import requests
 from bs4 import BeautifulSoup
+
+# Check the URL response
+try:
+  url = "https://volby.cz/pls/ps2017nss/ps3?xjazyk=CZ"
+  server_response = requests.get(url)
+  print(server_response.status_code)
+except:
+  print("Invalid URL address.")
