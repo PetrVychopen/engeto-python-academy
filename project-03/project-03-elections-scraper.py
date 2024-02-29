1f10569fe6ad832c4a6970aee041575f18c5106c
Petr Vychopeň <petr.vychopen@direct.cz>
2024-02-29 20:07:40 +0100

    feature - arguments

--- project-03/project-03-elections-scraper.py
+++ project-03/project-03-elections-scraper.py
@@ -6,16 +6,20 @@ email: vychopen.petr@gmail.com
 discord: Petr V. Psycholino#0224
 """
 import os
+import sys
 import requests
+from pprint import pprint
 from bs4 import BeautifulSoup
 
 # Clear screen
 os.system('cls')
 
-# Check the URL response
-try:
-  url = "https://volby.cz/pls/ps2017nss/ps3?xjazyk=CZ"
-  server_response = requests.get(url)
-  print(server_response.status_code)
-except:
-  print("Invalid URL address.")
+import sys
+
+requested_url = sys.argv[1]
+saved_file = sys.argv[2]
+
+# Feedback for user (url, filename)
+
+print(f"Requested URL: {requested_url}")
+print(f"Data will be saved to file: {saved_file}")
