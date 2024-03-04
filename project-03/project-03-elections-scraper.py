86a1d571b8a3786cb9ae6cca4a779128c4520cc6
Petr Vychopeň <petr.vychopen@direct.cz>
2024-03-04 19:41:50 +0100

    feature - parser

--- project-03/project-03-elections-scraper.py
+++ project-03/project-03-elections-scraper.py
@@ -5,6 +5,8 @@ author: Petr Vychopeň
 email: vychopen.petr@gmail.com
 discord: Petr V. Psycholino#0224
 """
+
+# Mandatory modules
 import os
 import sys
 import requests
@@ -14,6 +16,7 @@ from bs4 import BeautifulSoup
 # Clear screen
 os.system("cls")
 
+# Declaration of arguments
 requested_url = sys.argv[1]
 saved_file = sys.argv[2]
 
@@ -24,3 +27,12 @@ header_saved_file = f"Data will be saved to: {saved_file}"
 print(header_requested_url)
 print(header_saved_file)
 print("-" * len(header_requested_url))
+
+# Requested website response
+response = requests.get(requested_url)
+
+# Soup parser
+separated_html = BeautifulSoup(response.text, "html.parser")
+
+# TODO
+# print(separated_html.find_all("td"), {"class": "center"})
